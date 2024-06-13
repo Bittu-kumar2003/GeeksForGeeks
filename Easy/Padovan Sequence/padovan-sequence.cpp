c66db5a0694e4790835a7cfd12fa2b04c2b47a9e
Bittu kumar prajapati <109884531+Bittu-kumar2003@users.noreply.github.com>
2024-06-13 06:26:07 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Padovan Sequence/padovan-sequence.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    int padovanSequence(int n)
+    {
+       int dp[n+1];
+dp[0]=dp[1]=dp[2]=1;
+for(int i=3;i<=n;i++){
+dp[i] = (dp[i-2]+dp[i-3])%1000000007;
+}
+
+return dp[n];
+    }
+    
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	int n;
+    	cin>>n;
+    	Solution ob;
+    	cout<<ob.padovanSequence(n)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
